9a21d081cecd2ccb3e4bbb3aec02630ae14b952d
root <1559822619@qq.com>
2020-02-24 17:42:03 +0800

    spu商品信息保存和fastdfs图片上传服务完成

--- gmall-manage-web/src/main/java/com/fate/gmall/manager/controller/SpuController.java
+++ gmall-manage-web/src/main/java/com/fate/gmall/manager/controller/SpuController.java
@@ -3,11 +3,11 @@ package com.fate.gmall.manager.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.fate.gmall.bean.PmsProductInfo;
+import com.fate.gmall.manager.util.PmsUploadUtil;
 import com.fate.gmall.service.SpuService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -17,6 +17,27 @@ public class SpuController {
 
     @Reference
     SpuService spuService;
+
+
+
+    @RequestMapping("fileUpload")
+    @ResponseBody
+    public String fileUpload(@RequestParam("file") MultipartFile multipartFile){
+        // 将图片或者音视频上传到分布式的文件存储系统
+
+        // 将图片的存储路径返回给页面
+        String imgUrl = PmsUploadUtil.uploadImage(multipartFile);
+        System.out.println(imgUrl);
+
+        return imgUrl;
+    }
+    @RequestMapping("saveSpuInfo")
+    @ResponseBody
+    public String saveSpuInfo(@RequestBody PmsProductInfo pmsProductInfo){
+
+
+        return "success";
+    }
 
     @RequestMapping("spuList")
     @ResponseBody
