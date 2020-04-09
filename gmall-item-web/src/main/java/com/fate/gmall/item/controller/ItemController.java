52b69e47d304711e80564884b2c8bf5925a16f1b
root <1559822619@qq.com>
2020-04-09 17:52:51 +0800

    搜索功能实现

--- gmall-item-web/src/main/java/com/fate/gmall/item/controller/ItemController.java
+++ gmall-item-web/src/main/java/com/fate/gmall/item/controller/ItemController.java
@@ -2,8 +2,10 @@ package com.fate.gmall.item.controller;
 
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
 import com.fate.gmall.bean.PmsProductSaleAttr;
 import com.fate.gmall.bean.PmsSkuInfo;
+import com.fate.gmall.bean.PmsSkuSaleAttrValue;
 import com.fate.gmall.service.SkuService;
 import com.fate.gmall.service.SpuService;
 import org.springframework.stereotype.Controller;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 
@@ -33,6 +37,24 @@ public class ItemController {
         List<PmsProductSaleAttr> pmsProductSaleAttrs = spuService.spuSaleAttrListCheckBySku(pmsSkuInfo.getProductId(),skuId);
         map.put("spuSaleAttrListCheckBySku",pmsProductSaleAttrs);
 
+
+        //查询当前sku的spu的其他sku集合的hash表
+        Map<String,String> skuSaleAttrHash = new HashMap<>();
+        List<PmsSkuInfo> pmsSkuInfos = skuService.getSkuSaleAttrListBySpu(pmsSkuInfo.getProductId());
+        for (PmsSkuInfo skuInfo : pmsSkuInfos) {
+
+            String k = "";
+            String v = skuInfo.getId();
+            List<PmsSkuSaleAttrValue> skuSaleAttrValueList = skuInfo.getSkuSaleAttrValueList();
+            for (PmsSkuSaleAttrValue pmsSkuSaleAttrValue : skuSaleAttrValueList) {
+                k += pmsSkuSaleAttrValue.getSaleAttrValueId()+"|";
+
+            }
+            skuSaleAttrHash.put(k,v);
+
+        }
+        String skuSaleAttrHashJsonStr = JSON.toJSONString(skuSaleAttrHash);
+        map.put("skuSaleAttrHashJsonStr",skuSaleAttrHashJsonStr);
         return "item";
     }
 }
